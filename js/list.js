b348a3f1828a7afa256b176a0695dcd752a129e3
SmashRivals <63928998+SmashRivals@users.noreply.github.com>
2022-02-15 12:11:55 -0500

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,115 @@
+const d = {
+	"list": [
+    /*=================================================================================*/
+		
+    {
+		"vids": [
+        {
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": "1000FPS"
+				},
+			],
+                       "name": "Wistfully",
+                       "author": "xViola",
+                       "more": "KrXyt",
+                       "id": 67344125,
+                       "pass": "557774",
+                       "percentToQualify": "5",
+                       "verificationVid": "https://youtu.be/a4uVJWF-it4",
+                       "key": 0
+                },
+	
+  /*=================================================================================*/
+  
+  {
+		"vids": [
+        {
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": "3000 FPS"
+				},
+			],
+                       "name": "Singularity",
+                       "author": "070",
+                       "more": "070",
+                       "id": 0,
+                       "pass": "",
+                       "percentToQualify": "5",
+                       "verificationVid": "https://youtu.be/ZpeEs5PNAyQ?t=191",
+                       "key": 1
+                },
+  
+  /*=================================================================================*/
+  
+  
+  {
+		"vids": [
+        {
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": "1440 FPS"
+				},
+			],
+                       "name": "Arcturus",
+                       "author": "maxfs",
+                       "more": "maxfs",
+                       "id": 0,
+                       "pass": "",
+                       "percentToQualify": "5",
+                       "verificationVid": "https://youtu.be/Wi9ImtkDPG8",
+                       "key": 2
+                },
+  
+  /*=================================================================================*/
+  {
+		"vids": [
+        {
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": "720 FPS"
+				},
+			],
+                       "name": "Silent Circles 15",
+                       "author": "TheRealSailent",
+                       "more": "070",
+                       "id": 0,
+                       "pass": "pass",
+                       "percentToQualify": "5",
+                       "verificationVid": "https://www.youtube.com/watch?v=XfhxMBwJo7A",
+                       "key": 3
+                },
+   /*=================================================================================*/
+   {
+		"vids": [
+        {
+					"user": "",
+					"link": "",
+					"percent": "",
+					"hz": "1872 FPS"
+				},
+			],
+                       "name": "MINUSclub",
+                       "author": "xVOLUMNIUS",
+                       "more": "xVOLUMNIUS",
+                       "id": 0,
+                       "pass": "pass",
+                       "percentToQualify": "5",
+                       "verificationVid": "https://youtu.be/1SdZjtUaqV8",
+                       "key": 4
+                },
+   /*=================================================================================*/
+
+  ],
+	"version": [
+		2,
+		0, 
+		0
+	]
+};
+/*bottom right*/
+const list = d.list;const version = d.version;
